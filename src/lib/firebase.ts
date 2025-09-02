bca85ea95b495fc6f99b15dd81e58f090ea63423
keenn Gameplay <keennlemariem@gmail.com>
2025-09-02 12:57:07 +0000

    o erro persiste.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,25 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDWdh2PxShX7u8SKbbmxdP5zs8AYr31kcI",
   authDomain: "carcheck-gkeh4.firebaseapp.com",
   databaseURL: "https://carcheck-gkeh4-default-rtdb.firebaseio.com",
   projectId: "carcheck-gkeh4",
-  storageBucket: "carcheck-gkeh4.firebasestorage.app",
+  storageBucket: "carcheck-gkeh4.appspot.com",
   messagingSenderId: "886519139268",
   appId: "1:886519139268:web:04140f3c22c85be986f291",
   measurementId: "G-YB09K8E6X3"
 };
 
-// Initialize Firebase
+// Initialize Firebase and export the services
 export const app = initializeApp(firebaseConfig);
-
-// Initialize and export services
 export const db = getFirestore(app);
 export const auth = getAuth(app);
